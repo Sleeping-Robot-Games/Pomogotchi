1c4b8c07afb5c5045173a78d8e8bbe0328a07f5e
Bryan Reed <bryan87reed@gmail.com>
2022-11-21 22:24:08 -0700

    Timer prototype working

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,36 +1,67 @@
 extends Node2D
 
-var timer_mode = 'focus'
+var timer_mode = c.FOCUS
 var focus_theme_mode = ''
-var timer_seconds = 1500
+var timer_seconds =  c.STANDARD_FOCUS_SECONDS
 
 export(ButtonGroup) var focus_theme_group
 
 func _ready():
+	$Mode.text = timer_mode
+	update_display_time()
 	for button in focus_theme_group.get_buttons():
 		button.connect("pressed", self, "_on_FocusTheme_pressed")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func update_display_time():
-	var minutes = (int(timer_seconds) / 60) % 60 # Using modulo returns the number of mins
+	var minutes = (int(timer_seconds) / 60) % 60
 	var seconds = (int(timer_seconds)) % 60
-	$Time.text = "{min}:{sec}".format({"min": minutes, "sec": seconds})
+	$Time.text = "%02d:%02d" % [minutes, seconds]
 
 
 func _on_FocusTheme_pressed():
-	focus_theme_mode = focus_theme_group.get_pressed_button().name
-	
+	if timer_mode == c.BREAK or $Timer.is_stopped():
+		focus_theme_mode = focus_theme_group.get_pressed_button().name
 
 
 func _on_Start_button_up():
-	if focus_theme_mode:
-		$Timer.start()
-
+		if $Timer.is_stopped():
+			if focus_theme_mode:
+				# Start the timer if a focus theme is selected and update start button text
+				$Timer.start()
+				$Start.text = 'Pause'
+				# Disable the focus theme buttons while the timer runs
+				for button in focus_theme_group.get_buttons():
+						button.disabled = true
+		else:
+			$Timer.paused = !$Timer.paused
+			if $Timer.paused:
+				$Start.text = 'Resume'
+			else: 
+				$Start.text = 'Pause'
+			
 
 func _on_Timer_timeout():
 	timer_seconds -= 1
+	if timer_seconds == 0:
+		# Stop the timer when it gets to zero
+		$Timer.stop()
+		# Flip from Focus to Break or Vis Versa
+		timer_seconds = c.STANDARD_BREAK_SECONDS if timer_mode == c.FOCUS else c.STANDARD_FOCUS_SECONDS
+		timer_mode = c.BREAK if timer_mode == c.FOCUS else c.FOCUS
+		# Update the start button and mode text
+		$Start.text = 'Start'
+		$Mode.text = timer_mode
+		# Start the timer again automatically if just turned to break
+		if timer_mode == c.BREAK:
+			$Timer.start()
+			$Start.text = "Pause"
+			# Untoggle the focus theme button so a new one can be chosen
+			if focus_theme_group.get_pressed_button():
+				focus_theme_group.get_pressed_button().pressed = false
+			# Enable the focus theme buttons 
+			for button in focus_theme_group.get_buttons():
+					button.disabled = false
+			focus_theme_mode = ""
+		
 	update_display_time()
---
+++ scripts/constants.gd
@@ -0,0 +1,8 @@
+extends Node
+
+const FOCUS = 'Focus'
+const BREAK = 'Break'
+
+const STANDARD_FOCUS_SECONDS = 5 # 1500
+const STANDARD_BREAK_SECONDS = 3 # 300
+const LONG_BREAK_SECONDS = 900
