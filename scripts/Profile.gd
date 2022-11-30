3ddb93e223b824e83c0b75f3643ef2638f034858
Bryan Reed <bryan87reed@gmail.com>
2022-11-30 14:56:14 -0700

    useless code

--- scripts/Profile.gd
+++ scripts/Profile.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
 
-func _ready():
-	pass # Replace with function body.
 
+func _ready():
+	apply_xp()
 
 func _on_Back_button_up():
 	hide()
 	get_node('../Main').show()
+
+func apply_xp():
+	for xp_name in g.PET_STATE.xp:
+		var xp_node = get_node(xp_name)
+		for xp_bar in xp_node.get_node('XPContainer').get_children():
+			if int(xp_bar.name[2]) <= g.PET_STATE.xp[xp_name]:
+				print( int(xp_bar.name[2]))
+				xp_bar.texture = load('res://assets/uiArt/full-xp.png')
+			else:
+				xp_bar.texture = load('res://assets/uiArt/empty-xp.png')
+				
--- scripts/global.gd
+++ scripts/global.gd
@@ -7,6 +7,11 @@ const PET_STATE = {
 	'hunger': 0,
 	'happiness': 0,
 	'fp': 0,
+	'xp': {
+		'Strength': 0,
+		'Intelligence': 4,
+		'Luck': 1
+	}
 }
 
 func change_pet_state(state, num):
